f9e8b8c9dd0e28a9849a2de8c2da562096c07187
ZeLiang <zeliang.shao@gmail.com>
2020-06-03 00:01:23 +0800

    init project

---
+++ ElastosRTCDemo/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ElastosRTCDemo
+//
+//  Created by ZeLiang on 2020/6/2.
+//  Copyright © 2020 Elastos Foundation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Podfile
@@ -0,0 +1,17 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '11.0'
+use_frameworks!
+
+target 'ElastosRTC' do
+  pod 'GoogleWebRTC', '1.1.29400'
+  pod 'ElastosCarrierSDK', '5.5.1'
+
+  target 'ElastosRTCTests' do
+    inherit! :search_paths
+  end
+
+  target 'ElastosRTCDemo' do
+    inherit! :search_paths
+  end
+end
